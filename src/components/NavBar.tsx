bb72449b6a18953048904f484f8b9ac07a31ff22
ksrnnb <enrico.fermi.phys@gmail.com>
2021-10-31 20:40:19 +0900

    feat: ログアウトの実装 #18

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,59 @@
+import axios from 'axios';
 import {
     AppBar,
     Box,
     Toolbar,
+    Tooltip,
     Typography
 } from '@mui/material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { useHistory } from "react-router";
+import { logoutEndpoint } from '../route';
+import { useAuth, AuthContextInterface } from '../auth/ProvideAuth';
 
 interface NavBarProps {
     title: string
     needBackIcon?: boolean
 }
 
+const logout = async (auth: AuthContextInterface) => {
+    await axios.post(
+        logoutEndpoint,
+        {},
+        { withCredentials: true }
+    );
+
+    auth.signOut();
+}
+
 export default function NavBar(props: NavBarProps) {
     const history = useHistory();
+    const auth = useAuth();
     const handleBack = () => {
         history.goBack();
+    }
+
+    const handleLogout = () => {
+        logout(auth);
     }
 
     return (
         <AppBar position="static" enableColorOnDark={true}>
             <Toolbar>
-                {props.needBackIcon && 
-                    <ChevronLeftIcon onClick={handleBack}></ChevronLeftIcon>
+                {props.needBackIcon &&
+                    <Tooltip title="戻る">
+                        <ChevronLeftIcon sx={{ cursor: 'pointer' }} onClick={handleBack} />
+                    </Tooltip>
                 }
                 <Box sx={{ px: 1 }} />
-                <Typography variant="h5">
+                <Typography variant="h5" sx={{ flexGrow: 1 }}>
                     {props.title}
                 </Typography>
+                <Tooltip title="ログアウト">
+                    <LogoutIcon sx={{ cursor: 'pointer' }} onClick={handleLogout} />
+                </Tooltip>
             </Toolbar>
-        </AppBar>
+        </AppBar >
     );
 }
